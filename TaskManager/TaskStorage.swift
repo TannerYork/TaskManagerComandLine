9d2dc626d9e045735853ce2e935911716d40feaf
Tanner York <tanneryork@MacBook-Pro-4.local>
2018-09-19 08:02:02 -0500

    Outline

---
+++ TaskManager/TaskStorage.swift
@@ -0,0 +1,28 @@
+//
+//  TaskStorage.swift
+//  TaskManager
+//
+//  Created by Tanner York on 9/18/18.
+//  Copyright © 2018 Tanner York. All rights reserved.
+//
+
+import Foundation
+var taskStorage: [Task] = []
+class Storage {
+    //Method to create or add a task
+    func createTask() {
+        print("Nothing here yet")
+    }
+    
+    //Method to remove a task
+    func removeTask() {
+        print("Nothing here yet")
+    }
+    
+    //Method to update the task order based on priority
+    func updatePriority() {
+        print("Nothing here yet")
+    }
+    
+
+}
